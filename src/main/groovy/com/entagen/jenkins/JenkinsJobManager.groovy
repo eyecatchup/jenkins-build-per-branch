5bef5347b2af178348f555cfe9f29fbfe5de8508
nick <nick@eleven.nl>
2015-10-03 16:54:53 +0200

    Refactored plugin: - Removed all the complicated view logic: the view name is now used as direct parent of the build jobs.

--- src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
+++ src/main/groovy/com/entagen/jenkins/JenkinsJobManager.groovy
@@ -10,7 +10,7 @@ class JenkinsJobManager {
     String viewRegex
     String jenkinsUser
     String jenkinsPassword
-    
+
     Boolean dryRun = false
     Boolean noViews = false
     Boolean noDelete = false
@@ -37,11 +37,6 @@ class JenkinsJobManager {
 
         // create any missing template jobs and delete any jobs matching the template patterns that no longer have branches
         syncJobs(allBranchNames, allJobNames, templateJobs)
-
-        // create any missing branch views, scoped within a nested view if we were given one
-        if (!noViews) {
-            syncViews(allBranchNames)
-        }
     }
 
     public void syncJobs(List<String> allBranchNames, List<String> allJobNames, List<TemplateJob> templateJobs) {
@@ -60,11 +55,15 @@ class JenkinsJobManager {
 
     public void createMissingJobs(List<ConcreteJob> expectedJobs, List<String> currentJobs, List<TemplateJob> templateJobs) {
         List<ConcreteJob> missingJobs = expectedJobs.findAll { !currentJobs.contains(it.jobName) }
-        if (!missingJobs) return
+        if (!missingJobs) {
+            println "No missing jobs to create.."
+            return
+        }
 
-        for(ConcreteJob missingJob in missingJobs) {
+        for (ConcreteJob missingJob in missingJobs) {
             println "Creating missing job: ${missingJob.jobName} from ${missingJob.templateJob.jobName}"
-            jenkinsApi.cloneJobForBranch(missingJob, templateJobs)
+            jenkinsApi.cloneJobForBranch(this.nestedView, missingJob, templateJobs)
+
             if (startOnCreate && !startExpected) {
                 jenkinsApi.startJob(missingJob)
             }
@@ -102,7 +101,7 @@ class JenkinsJobManager {
 
         // don't want actual template jobs, just the jobs that were created from the templates
         return (allJobNames - templateJobNames).findAll { String jobName ->
-            templateBaseJobNames.find { String baseJobName -> jobName.startsWith(baseJobName)}
+            templateBaseJobNames.find { String baseJobName -> jobName.startsWith(baseJobName) }
         }
     }
 
@@ -121,39 +120,6 @@ class JenkinsJobManager {
         assert templateJobs?.size() > 0, "Unable to find any jobs matching template regex: $regex\nYou need at least one job to match the templateJobPrefix and templateBranchName suffix arguments"
 
         return templateJobs
-    }
-
-    public void syncViews(List<String> allBranchNames) {
-        List<String> existingViewNames = jenkinsApi.getViewNames(this.nestedView)
-        List<BranchView> expectedBranchViews = allBranchNames.collect { String branchName -> new BranchView(branchName: branchName, templateJobPrefix: this.templateJobPrefix) }
-
-        List<BranchView> missingBranchViews = expectedBranchViews.findAll { BranchView branchView -> !existingViewNames.contains(branchView.viewName)}
-        addMissingViews(missingBranchViews)
-
-        if (!noDelete) {
-            List<String> deprecatedViewNames = getDeprecatedViewNames(existingViewNames, expectedBranchViews)
-            deleteDeprecatedViews(deprecatedViewNames)
-        }
-    }
-
-    public void addMissingViews(List<BranchView> missingViews) {
-        println "Missing views: $missingViews"
-        for (BranchView missingView in missingViews) {
-            jenkinsApi.createViewForBranch(missingView, this.nestedView, this.viewRegex)
-        }
-    }
-
-    public List<String> getDeprecatedViewNames(List<String> existingViewNames, List<BranchView> expectedBranchViews) {
-         return existingViewNames?.findAll { it.startsWith(this.templateJobPrefix) } - expectedBranchViews?.viewName ?: []
-    }
-
-    public void deleteDeprecatedViews(List<String> deprecatedViewNames) {
-        println "Deprecated views: $deprecatedViewNames"
-
-        for(String deprecatedViewName in deprecatedViewNames) {
-            jenkinsApi.deleteView(deprecatedViewName, this.nestedView)
-        }
-
     }
 
     JenkinsApi initJenkinsApi() {
@@ -176,7 +142,7 @@ class JenkinsJobManager {
         if (!gitApi) {
             assert gitUrl != null
             this.gitApi = new GitApi(gitUrl: gitUrl)
-            if (this.branchNameRegex){
+            if (this.branchNameRegex) {
                 this.gitApi.branchNameFilter = ~this.branchNameRegex
             }
         }
--- src/main/groovy/com/entagen/jenkins/Main.groovy
+++ src/main/groovy/com/entagen/jenkins/Main.groovy
@@ -11,7 +11,7 @@ class Main {
             u: [longOpt: 'git-url',  required: true, args: 1, argName: 'gitUrl', description: "Git Repository URL - gradle flag -DgitUrl=<gitUrl>"],
             p: [longOpt: 'job-prefix', required: true, args: 1, argName: 'templateJobPrefix', description: "Template Job Prefix, - gradle flag -DtemplateJobPrefix=<jobPrefix>"],
             t: [longOpt: 'template-branch', required: true, args: 1, argName:  'templateBranchName', description: "Template Branch Name - gradle flag -DtemplateBranchName=<branchName>"],
-            n: [longOpt: 'nested-view', required: false, args: 1, argName: 'nestedView', description: "Nested Parent View Name - gradle flag -DnestedView=<nestedView> - optional - must have Jenkins Nested View Plugin installed"],
+            n: [longOpt: 'nested-view', required: true, args: 1, argName: 'nestedView', description: "Parent View Name - gradle flag -DnestedView=<nestedView> - optional - must have Jenkins Nested View Plugin installed"],
             c: [longOpt: 'print-config', required: false, args: 0, argName: 'printConfig', description:  "Check configuration - print out settings then exit - gradle flag -DprintConfig=true"],
             d: [longOpt: 'dry-run', required: false, args: 0, argName: 'dryRun', description:  "Dry run, don't actually modify, create, or delete any jobs, just print out what would happen - gradle flag: -DdryRun=true"],
             s: [longOpt: 'start-on-create', required: false, args: 0, argName: 'startOnCreate', description:  "When creating a new job, start it at once."],
